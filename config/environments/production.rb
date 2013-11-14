36f86e5681fc65f6feef28c284776b0b7fc3d10f
Allen <iamsuperali@126.com>
2013-11-14 10:25:17 +0800

    assets.precompile 中加入 admin.js 和 admin.css

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,7 +46,7 @@ FishCms::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  # config.assets.precompile += %w( search.js )
+  config.assets.precompile += %w( admin.js admin.css)
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
@@ -64,6 +64,7 @@ FishCms::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+  
 
   #devise配置
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
